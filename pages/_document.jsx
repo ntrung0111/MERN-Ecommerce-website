353c2f4dbe42c45f4e54a063b373232ea707a824
NguyenTienTrung <4501104262@student.hcmue.edu.vn>
2022-11-18 11:13:49 +0700

    update

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -50,7 +50,7 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
           `,
